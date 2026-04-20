ce15e6c0a32d13404ad57cb3f145e774b149b1a6
Malinda <greshanamarakoon@gmail.com>
2026-04-20 13:57:39 +0530

    base window setup

--- queens/app.py
+++ queens/app.py
@@ -5,3 +5,17 @@ from database import DatabaseManager
 from logic import NQueensLogic
 
 MAX_SOLUTIONS = 30
+
+
+class ChessApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("16 Queens Puzzle")
+
+        self.root.geometry("1100x800")
+
+        self.db = DatabaseManager()
+        self.logic = NQueensLogic(16)
+        self.selected_queens = []
+
+        self.setup_ui()
