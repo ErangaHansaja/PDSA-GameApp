9979f67187564efbd149a260df24e40cd032e6db
Y A M R Perera <malieshaperera@gmail.com>
2026-04-21 21:16:05 +0530

    Add sequential and threaded solving methods

--- queens/logic.py
+++ queens/logic.py
@@ -29,3 +29,56 @@ class NQueensLogic:
             temp = current + [(row, col)]
             if self.is_valid(temp):
                 self.backtrack_solver(row + 1, temp, results, solutions)
+
+def run_sequential(self):
+        results = [0]
+        solutions = []
+        start = time.time()
+
+        self.backtrack_solver(0, [], results, solutions)
+
+        return results[0], time.time() - start, solutions
+
+    def run_threaded(self):
+        total = [0]
+        solutions = []
+        lock = threading.Lock()
+        threads = []
+        start = time.time()
+
+        def task(start_col):
+            local_solutions = []
+
+            def limited_backtrack(row, current):
+                # STOP if global limit reached
+                with lock:
+                    if total[0] >= self.max_solutions:
+                        return
+
+                if row == self.size:
+                    with lock:
+                        if total[0] < self.max_solutions:
+                            total[0] += 1
+                            solutions.append(current.copy())
+                    return
+
+                for col in range(self.size):
+                    temp = current + [(row, col)]
+
+                    if self.is_valid(temp):
+                        limited_backtrack(row + 1, temp)
+
+            limited_backtrack(1, [(0, start_col)])
+
+        # Create threads
+        for c in range(self.size):
+            t = threading.Thread(target=task, args=(c,))
+            threads.append(t)
+            t.start()
+
+        # Wait for all
+        for t in threads:
+            t.join()
+
+        return total[0], time.time() - start, solutions
+
