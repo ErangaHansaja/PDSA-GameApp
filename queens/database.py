68afacaec37d2e2d0d9c788927b6e06551be8f49
Y A M R Perera <malieshaperera@gmail.com>
2026-04-21 17:34:34 +0530

    Add performance stats saving and player response methods

--- queens/database.py
+++ queens/database.py
@@ -55,3 +55,28 @@ class DatabaseManager:
         cursor.execute("INSERT INTO Solutions VALUES (?)", (solution,))
         conn.commit()
         conn.close()
+
+ def save_performance_stats(self, s_count, t_count, s_time, t_time):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO Program_Respond (sequential, threaded, time_taken_seq, time_taken_thread) VALUES (?,?,?,?)",
+            (s_count, t_count, s_time, t_time),
+        )
+        conn.commit()
+        conn.close()
+
+    def get_player_solution_count(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM Player_Responses")
+        result = cursor.fetchone()[0]
+        conn.close()
+        return result
+
+    def clear_player_responses(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Player_Responses")
+        conn.commit()
+        conn.close()
